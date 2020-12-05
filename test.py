93c54e4210d1c08b8291c7865a221d184209e767
ilazy991 <616119901@qq.com>
2020-12-05 13:57:27 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
+from selenium import webdriver
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+driver = webdriver.Chrome(options=chrome_options)
+
 print(123)
